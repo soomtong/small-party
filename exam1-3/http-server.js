09fa15af7538b54035bfd5b2c0e358e656150534
Aiden <soomtong@gmail.com>
2019-01-15 22:01:30 +0900

    added cors header

--- exam1-3/http-server.js
+++ exam1-3/http-server.js
@@ -11,6 +11,15 @@ const server = http.createServer(function(req, res) {
 	}
 
 	saveAccessLog(data)
+	
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Request-Method', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+	res.setHeader('Access-Control-Allow-Headers', '*')
+	if ( req.method === 'OPTIONS' ) {
+		res.writeHead(200)
+		return res.end()
+	}
 
 	res.writeHead(200, { 'Content-Type': 'text/html' })
 	res.write(`<p>${msg}</p>`)
